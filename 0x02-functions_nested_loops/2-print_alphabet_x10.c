1ce27a320d647da6f5f89fb9beeaf7e1f1c02ea6
khalid357 <khalidadebowale45@gmail.com>
2022-10-19 11:54:50 -0700

    C language is interesting

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,10 @@
 #include "main.h"
 
-
-
 /**
+ * print_alphabet_x10 - Make alphabet x10 times
  *
- *  * print_alphabet_x10 - Make alphabet x10 times
- *
- *   *
- *
- *    * Return: void
- *
- *     */
+ * Return: void
+ */
 
 void print_alphabet_x10(void)
 {
