bf6b6dd77334a86332febf3f67524c90c0016ac4
khalid357 <khalidadebowale45@gmail.com>
2022-10-22 04:04:22 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- *
+ * print_to_98 - print all numbers from inout to 98
+ * @n: starting the number
  */
 
-int main(void)
+void print_to_98(int n)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--)
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++)
+		printf("%d\n", n)	;	
+	}
 }
