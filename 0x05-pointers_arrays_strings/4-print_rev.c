88aaa7d41afc080311d6915fe35f4c4fc7c839e5
khalid357 <khalidadebowale45@gmail.com>
2022-10-24 03:51:45 -0700

    alx - low level programming

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int a;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (a = length; a > 0; a--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n')
+}
