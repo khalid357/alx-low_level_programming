680b2ba3d0c0215f048604431cca7894370ce2c7
khalid357 <khalidadebowale45@gmail.com>
2022-10-27 21:43:46 -0700

    Starting to code

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of array
+ * Returb: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < n--; x++)
+	{
+		x = a[i];
+		t = a[i];
+		a[n] = t;
+	}
+}
+
+
